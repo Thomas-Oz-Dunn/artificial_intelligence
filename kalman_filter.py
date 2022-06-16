55c7aded513754054509002c23be031269cb7ad7
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-06-15 20:57:28 -0500

    kalman_filter.py: create class, sep functions

--- kalman_filter.py
+++ kalman_filter.py
@@ -3,61 +3,106 @@ Useful functions for Kalman Filter
 """
 import numpy as np
 
+THRESHOLD = 1e-6
+
 def interact_multi_model(
-    input_state,
-    input_covariance
+    mean_state, 
+    mean_cov
 ):
-    # Predict and update loop
-    while err > THRESHOLD
-        predict()
-        update()
-    return (output_state, output_covariance)
+    return (mean_state, mean_cov)
+
+class KalmanFilter:
+    """
+    Kalman Filter class
+    """
+    def __init__(self, mean_state, mean_cov):
+        self.mean_state = mean_state
+        self.mean_cov = mean_cov
+        self.transition_matrix = 
+        self.prcx_noise_cov = np.eye(mean_state.shape() [0])
+        self.input_effect = np.eye(mean_state.shape() [0])
+        self.control_input = np.zeros((mean_state.shape() [0], 1))
+        self.measure_matrix = 
+        self.measure_cov =        
+
+    def run(
+        self, 
+        new_measure,
+        n_iter: int = 150):
+        
+        for i_iter in np.arange(0, n_iter):
+            
+            (mean_state, mean_cov) = predict(
+                self.mean_state, 
+                self.mean_cov, 
+                self.transition_matrix, 
+                self.prcx_noise_cov, 
+                self.input_effect, 
+                self.control_input)
+            (mean_state, mean_cov) = update(
+                self.mean_state, 
+                self.mean_cov, 
+                new_measure, 
+                self.measure_matrix, 
+                self.measure_cov)
+        
+        return (mean_state, mean_cov)
 
 def predict(
-    prev_state, 
-    prev_cov, 
-    transition, 
+    mean_state, 
+    mean_cov, 
+    transition_matrix, 
     prcx_noise_cov, 
-    input_effect, 
+    control_effect, 
     control_input
 ):
-  new_state = np.dot(transition, prev_state) + np.dot(input_effect, control_input)
-  new_cov = np.dot(transition, np.dot(prev_cov, transition.T)) + prcx_noise_cov
-  return (new_state, new_cov)
+    predicted_state = predict_state(
+        mean_state=mean_state,
+        transition_matrix=transition_matrix,
+        control_effect=control_effect,
+        control_input=control_input)
+
+    predicted_covariance = predict_covariance(
+        mean_covariance=mean_cov,
+        transition_matrix=transition_matrix,
+        prcx_noise_covariance=prcx_noise_cov)
+
+    return (predicted_state, predicted_covariance)
+
+
+def predict_covariance(
+    mean_covariance,
+    transition_matrix,
+    prcx_noise_covariance
+):
+    transition_cov = np.dot(mean_covariance, transition_matrix.T)
+    return np.dot(transition_matrix, transition_cov) + prcx_noise_covariance
+
+
+def predict_state(
+    mean_state,
+    transition_matrix,
+    control_effect,
+    control_input
+):
+    transition = np.dot(transition_matrix, mean_state)
+    controls = np.dot(control_effect, control_input)
+    return transition + controls
+
 
 def update(
-    prev_state, 
-    prev_cov, 
-    new_meas, 
-    meas_matr, 
-    meas_cov
+    mean_state, 
+    mean_cov, 
+    new_measure, 
+    measure_matrix, 
+    measure_cov
 ):
-  IM = np.dot(meas_matr, prev_state)
-  IS = meas_cov + np.dot(meas_matr, np.dot(prev_cov, meas_matr.T))
-  K = np.dot(prev_cov, np.dot(meas_matr.T, np.linalg.inv(IS)))
-  X = prev_state + np.dot(K, (new_meas - IM))
-  P = prev_cov - np.dot(K, np.dot(IS, K.T))
-  LH = gauss_pdf(new_meas, IM, IS)
-  return (X, P, K, IM, IS, LH)
+    pred_mean = np.dot(measure_matrix, mean_state)
+    predict_cov = measure_cov + np.dot(measure_matrix, np.dot(mean_cov, measure_matrix.T))
+    kalman_gain = np.dot(mean_cov, np.dot(measure_matrix.T, np.linalg.inv(predict_cov)))
+    diff = (new_measure - pred_mean)
 
-def gauss_pdf(
-    X, 
-    mean, 
-    covariance
-):
-  if mean.shape()[1] == 1:
-    DX = X - np.tile(mean, X.shape()[1])
-    E = 0.5 * sum(DX * (np.dot(np.linalg.inv(covariance), DX)), axis=0)
-    E = E + 0.5 * mean.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
-    P = np.exp(-E)
-  elif X.shape()[1] == 1:
-    DX = np.tile(X, mean.shape()[1]) - mean
-    E = 0.5 * sum(DX * (np.dot(np.linalg.inv(covariance), DX)), axis=0)
-    E = E + 0.5 * mean.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(covariance))
-    P = np.exp(-E)
-  else:
-    DX = X - mean
-    E = 0.5 * np.dot(DX.T, np.dot(np.linalg.inv(covariance), DX))
-    E = E + 0.5 * mean.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(covariance))
-    P = np.exp(-E)
-  return (P[0], E[0])
+    mean_state = mean_state + np.dot(kalman_gain, diff)
+    mean_cov = mean_cov - np.dot(kalman_gain, np.dot(predict_cov, kalman_gain.T))
+
+    return (mean_state, mean_cov)
