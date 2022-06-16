f421905e2e4181ec3b5f010d0120cf26998d1718
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-06-15 20:58:09 -0500

    AEIMM.py: formatting update

--- AEIMM.py
+++ AEIMM.py
@@ -15,17 +15,42 @@ class Denoise(keras.Model):
     super(Denoise, self).__init__()
     self.encoder = keras.Sequential([
       keras.layers.Input(shape=(28, 28, 1)),
-      keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', strides=2),
-      keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', strides=2)])
+      keras.layers.Conv2D(
+        256, 
+        (3, 3), 
+        activation='relu', 
+        padding='same', 
+        strides=2),
+      keras.layers.Conv2D(
+        64, 
+        (3, 3), 
+        activation='relu',
+         padding='same', 
+         strides=2)])
 
     self.decoder = keras.Sequential([
-      keras.layers.Conv2DTranspose(64, kernel_size=3, strides=2, activation='relu', padding='same'),
-      keras.layers.Conv2DTranspose(256, kernel_size=3, strides=2, activation='relu', padding='same'),
-      keras.layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')])
+      keras.layers.Conv2DTranspose(
+        64, 
+        kernel_size=3, 
+        strides=2, 
+        activation='relu', 
+        padding='same'),
+      keras.layers.Conv2DTranspose(
+        256, 
+        kernel_size=3, 
+        strides=2, 
+        activation='relu', 
+        padding='same'),
+      keras.layers.Conv2D(
+        1, 
+        kernel_size=(3, 3), 
+        activation='sigmoid', 
+        padding='same')])
 
   def call(self, x):
     encoded = self.encoder(x)
     # Insert IMM here
+    # Input R, process covariance, and x, the measurement
     kf.interact_multi_model(encoded)
     decoded = self.decoder(encoded)
     return decoded
