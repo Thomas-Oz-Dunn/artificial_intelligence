e78f3cecfb407c0e4467c519fc35dc0e09f9834a
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-07-03 15:56:55 -0500

    IMM.py: update interactive Multi Model class

--- IMM.py
+++ IMM.py
@@ -4,80 +4,36 @@ Interacting Multi Model
 """
 
 import numpy as np
-import kalman_filter.KalmanFilter as KalmanFilter
+import kalman_filter as kf
 
-def interact_multi_model(
-    state, 
-    covariance,
-    time_step,
-    omega,
-    measurement
-):
-    # [x_pos, y_pos, x_vel, y_vel]
-    cons_vel_mat = np.array([
-        [1, 0, time_step, 0],
-        [0, 1, 0, time_step],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]])
-    const_vel_KF = KalmanFilter(
-        state=state,
-        covariance=covariance,
-        transition_matrix=cons_vel_mat)
+class InteractiveMultiModel:
+    """
+    A class for Interactive MultiModel (IMM)
+    """
+    def __init__(
+        self,
+        filters: kf.KalmanFilter,
+        confidences,
+        markov_transition
+    ) -> None:
+        self.filter = filters
+        self.MU = confidences
+        self.markov_transition = markov_transition
 
-    # [x_pos, y_pos, x_vel, y_vel, x_acc, y_acc]
-    cons_acc_mat = np.array([
-        [1, 0, time_step, 0, 0.5 * time_step**2, 0],
-        [0, 1, 0, time_step, 0, 0.5 * time_step**2],
-        [0, 0, 1, 0, time_step, 0],
-        [0, 0, 0, 1, 0, time_step],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1]])
-    const_acc_KF = KalmanFilter(
-        state=state,
-        covariance=covariance,
-        transition_matrix=cons_acc_mat)
-    
-    # [x_pos, y_pos, x_vel, y_vel, omega]
-    cons_turn_mat = np.array([
-        [1, 0, np.sin(omega*time_step)/omega, -(1-np.cos(omega*time_step))/omega, 0],
-        [0, 1, np.cos(omega*time_step), -np.sin(omega*time_step), 0],
-        [0, 0, (1-np.cos(omega*time_step))/omega, np.sin(omega*time_step)/omega, 0],
-        [0, 0, np.sin(omega*time_step), -np.cos(omega*time_step), 0],
-        [0, 0, 0, 0, 1]])
-    const_turn_KF = KalmanFilter(
-        state=state,
-        covariance=covariance,
-        transition_matrix=cons_turn_mat)
+    def compute_state_estimate(self) -> None:
+        for f, mu in zip(self.filters, self.MU):
+            self.state += f.state * mu
 
-    state_cv, cov_cv = const_vel_KF.run(
-        new_measure=measurement,
-        measure_cov=,
-        measure_matrix=)
-    state_ca, cov_ca = const_acc_KF.run(
-        new_measure=measurement,
-        measure_cov=,
-        measure_matrix=)
-    state_ct, cov_ct = const_turn_KF.run(
-        new_measure=measurement,
-        measure_cov=,
-        measure_matrix=)
-    
-    # Markov Decision Process
-    mixed_trans = 
-    mixed_state =
-    mixed_cov = 
+        for f, mu in zip(self.filters, self.MU):
+            y = f.state - self.state
+            self.p += mu * (np.outer(y, y) + f.covariance)
 
-    mixed_KF = KalmanFilter(
-        state=mixed_state,
-        covariance=mixed_cov,
-        transition_matrix=mixed_trans)
+    def compute_mixing_probabilities(self) -> None:
+        N = len(self.confidences)
+        omega = np.zeros([N, N])
+        cbar = np.dot(self.confidences, self.transition)
+        for i in N:
+            for j in N:
+                omega[i, j] = self.markov_transition[i, j] * self.MU[i] / cbar[j]
 
-    state, covariance = mixed_KF.run(
-        new_measure=measurement,
-        measure_matrix=,
-        measure_cov=)
-
-    # Model comparison based an accuracy to ground truth
-    # Apply weight to prediction for decoder
-
-    return (state, covariance)
+        return omega
